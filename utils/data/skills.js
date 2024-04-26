05b2293cff56bdabc9a8389e42a9900cb7e34040
Cole Cochran <colecochran405@gmail.com>
2024-04-26 16:45:01 -0500

    adding new skills svgs for terraform and Ubuntu

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -1,28 +1,28 @@
 export const skillsData = [
-  'HTML',
-  'CSS',
-  'Javascript',
-  'Typescript',
-  'React',
-  'Next JS',
-  'Tailwind',
-  'MongoDB',
-  'MySQL',
-  'PostgreSQL',
-  'Git',
-  'AWS',
-  'GCP',
-  'Bootstrap',
-  'Docker',
-  'Figma',
-  'MaterialUI',
-  'Nginx',
-  'Ubuntu',
-]
+  "HTML",
+  "CSS",
+  "Javascript",
+  "Typescript",
+  "React",
+  "Next JS",
+  "Tailwind",
+  "MongoDB",
+  "MySQL",
+  "PostgreSQL",
+  "Git",
+  "AWS",
+  "GCP",
+  "Bootstrap",
+  "Docker",
+  "Figma",
+  "MaterialUI",
+  "Nginx",
+  "Ubuntu",
+  "Terraform",
+];
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
 // Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
-
 
 // AVAILABLE SKILLS
 
@@ -105,4 +105,5 @@ export const skillsData = [
   Tensorflow
   Webex
   Wordpress
+  Terraform
 */
