a5e74fdd90ab03a1a7332205bb812648e7e55503
Cole Cochran <colecochran405@gmail.com>
2024-04-26 12:51:27 -0500

    adding my developer information

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,17 +1,19 @@
 export const personalData = {
-  name: "ABU SAID",
-  profile: '/profile.png',
+  name: "Cole Cochran",
+  profile: "/profile.png",
   designation: "Software Developer",
-  description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'abusaid7388@gmail.com',
-  phone: '+8801608797655',
-  address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-  github: 'https://github.com/said7388',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  leetcode: "https://leetcode.com/said3812/",
-  devUsername: "said7388",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
-}
+  description:
+    "My name is Cole Cochran. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: "colecochran405@gmail.com",
+  phone: "+1(512)827-6219",
+  address: "Middle Badda, Austin, Texas - 1212 ",
+  github: "https://github.com/Cochran7388",
+  facebook: "https://www.facebook.com/ColeCochran.riyaz/",
+  linkedIn: "https://www.linkedin.com/in/Cole-Cochran-bd/",
+  twitter: "https://twitter.com/Cochran7388",
+  stackOverflow: "https://stackoverflow.com/users/16840768/Cole-Cochran",
+  leetcode: "https://leetcode.com/Cochran3812/",
+  devUsername: "Cochran7388",
+  resume:
+    "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
+};
